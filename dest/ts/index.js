b51f6c185c10c7b604f58f3ed4a2f5f6e5908bb2
Jason Smith <j50n@lilypepper.com>
2019-02-17 23:42:40 -0700

    site

--- dest/ts/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const immutable_1 = require("immutable");
-console.log("--------------------------- GOT HERE -----------------------------------------------");
-console.log(`LIST WORKS! ${immutable_1.List([1, 2, 3])}`);
-//# sourceMappingURL=index.js.map
--- ts/index.ts
+++
@@ -1,4 +0,0 @@
-import { List } from "immutable";
-
-console.log("--------------------------- GOT HERE -----------------------------------------------");
-console.log(`LIST WORKS! ${List([1, 2, 3])}`);
