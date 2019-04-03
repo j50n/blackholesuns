9d4eff3749833eb765aa5dbd4dd2a01d4345a913
Jason Smith <j50n@lilypepper.com>
2019-04-02 19:04:07 -0600

    make

--- make.sh
+++ make.sh
@@ -4,13 +4,16 @@ set -e
 HERE="$(realpath $(dirname $0))"
 
 cd "$HERE/common"
+rm -rf "$HERE/common/node_modules"
 npm update
 npm run build
 
 cd "$HERE/cli"
+rm -rf "$HERE/cli/node_modules/"
 npm update
 npm run build
 
 cd "$HERE/website"
+rm -rf "$HERE/website/node_modules/"
 npm update
 npm run build
