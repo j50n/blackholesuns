370b987322df69053c601c856d4f49945f7bace8
Jason Smith <j50n@lilypepper.com>
2019-06-09 15:26:23 -0600

    removed obsolete code

--- update.sh
+++ update.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 set -e
-set -x
+# set -x
 
 TARGET="blackholesuns.gh-pages"
-TARGET_STAGING="blackholesuns-staging.gh-pages"
+# TARGET_STAGING="blackholesuns-staging.gh-pages"
 
 HERE="$(realpath $(dirname $0))"
 PAGES="$(realpath "$HERE/../$TARGET")"
-PAGES_STAGING="$(realpath "$HERE/../$TARGET_STAGING")"
+# PAGES_STAGING="$(realpath "$HERE/../$TARGET_STAGING")"
 
 cd "$HERE/cli" && (
     npm run all   
 )
 
 cp "$HERE/website/public/blackholes.txt" "$PAGES/"
-cp "$HERE/website/public/blackholes.txt" "$PAGES_STAGING/"
+# cp "$HERE/website/public/blackholes.txt" "$PAGES_STAGING/"
 
 cd "$PAGES" && (
     git commit -am "data update `date`"
     git push
 )
 
-cd "$PAGES_STAGING" && (
-    git commit -am "data update `date`"
-    git push
-)
+# cd "$PAGES_STAGING" && (
+#     git commit -am "data update `date`"
+#     git push
+# )
 
 echo "TOTAL: $(cat "$PAGES/blackholes.txt" | wc -l)"
